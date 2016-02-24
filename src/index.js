1fbbf889070017894e86c41a77cbd0d0759db172
Julien Pradet <julien@pradet.me>
2016-02-24 20:19:01 +0100

    Fix mars-rover

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ var keyDown$ = rx.Observable.fromEvent(document, 'keydown')
       action: 'down',
       key: e.key || e.which
     }
-  });
+  })
 
 var keyUp$ = rx.Observable.fromEvent(document, 'keyup')
   .map(function(e) {
@@ -16,43 +16,51 @@ var keyUp$ = rx.Observable.fromEvent(document, 'keyup')
       action: 'up',
       key: e.key || e.which
     }
-  });
+  })
 
 var pressedKeys$ = rx.Observable.merge(keyDown$, keyUp$)
   .scan(
-    function(acc, action) {
-      acc[action.key] = action.action === 'down'
-      return acc;
+    function(pressedKeys, keyEvent) {
+      pressedKeys[keyEvent.key] = keyEvent.action === 'down'
+      return pressedKeys;
     },
     {}
   )
+  .map(function(pressedKeys) {
+    return Object.keys(pressedKeys)
+      .filter(function(key) {
+        return pressedKeys[key];
+      })
+  })
 
 pressedKeys$.subscribe(function(pressedKeys) {
-  var pressedKeys = Object.keys(pressedKeys)
-    .filter(function(key) { return pressedKeys[key] })
-    .join(', ');
-  document.getElementById('pressed-keys').innerHTML = pressedKeys
+  document.getElementById('pressed-keys').innerHTML = pressedKeys.join(',')
 })
 
-var action$ = keyDown$
-  .pluck('key')
+const keyToAction = {
+  38: 'forward',
+  40: 'backward',
+  37: 'rotateLeft',
+  39: 'rotateRight',
+}
+
+var action$ = pressedKeys$
+  .flatMap(function(pressedKeys) {
+    return pressedKeys
+  })
+  .filter(function(key) {
+    return keyToAction.hasOwnProperty(key)
+  })
   .map(function(key) {
-    if(key === 38) {
-      return 'forward'
-    } else if(key === 40) {
-      return 'backward'
-    } else if(key === 37) {
-      return 'rotateLeft'
-    } else if(key === 39) {
-      return 'rotateRight'
-    }
+    console.log(key);
+    return keyToAction[key]
   })
 
 var rover$ = action$.scan(
   function(rover, action) {
     return rover[action]()
   },
-  Rover(Point(0, 0), 'E')
+  Rover(Point(0, 0), 'N')
 )
 
 rover$.subscribe(function(rover) {
